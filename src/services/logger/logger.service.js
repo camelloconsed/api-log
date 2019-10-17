ab16ece7d773c4b690063764bbb5a8b6b84c926c
Joaquin Navarro <rexxar1989@gmail.com>
2019-10-17 16:09:55 -0300

    [[ FIX ]] was removed console.log from logger.service

--- src/services/logger/logger.service.js
+++ src/services/logger/logger.service.js
@@ -4,23 +4,23 @@ export default () => {
   const logger = {};
 
   logger.show = async (params) => {
-    console.log(params);
+    
     return params;
   };
   logger.list = async (params) => {
-    console.log(params);
+    
     return params;
   };
   logger.store = async (params) => {
-    console.log(params);
+    
     return params;
   };
   logger.update = async (params) => {
-    console.log(params);
+    
     return params;
   };
   logger.delete = async (params) => {
-    console.log(params);
+    
     return params;
   };
   return logger;
