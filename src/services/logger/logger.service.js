7d7b1c6f8d4d874b52eb3a92368d0aba5a29a875
Joaquin Navarro <rexxar1989@gmail.com>
2019-10-17 16:16:05 -0300

    [[ FIX ]] was modified errors space blank logger.service.js

--- src/services/logger/logger.service.js
+++ src/services/logger/logger.service.js
@@ -4,23 +4,18 @@ export default () => {
   const logger = {};
 
   logger.show = async (params) => {
-    
     return params;
   };
   logger.list = async (params) => {
-    
     return params;
   };
   logger.store = async (params) => {
-    
     return params;
   };
   logger.update = async (params) => {
-    
     return params;
   };
   logger.delete = async (params) => {
-    
     return params;
   };
   return logger;
