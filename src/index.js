d120aeeb9aba3407d16f8756f5928e5a81daa49e
Joaquin Navarro <rexxar1989@gmail.com>
2019-10-24 14:50:16 -0300

    [FEAT] Was added connection to database mongoDB with docker index.js was modified package.json script start index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const koaBody = require('koa-body');
 const json = require('koa-json');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 const router = new KoaRouter();
 
 const logger = Log();
+
+mongoose
+  .connect('mongodb://juako:123123@127.0.0.1:27017/logDB', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB Connected!'))
+  .catch(err => {
+    console.log('DB disconnected');
+  });
 
 app.use(json());
 
